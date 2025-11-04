e09da7edf6b15d6a4da6a123a89c3436b423eff7
eric761231 <eric761231@users.noreply.github.com>
2025-11-04 16:09:05 +0800

    Add Google OAuth Client ID for Netlify deployment

---
+++ html/js/config.js
@@ -0,0 +1,8 @@
+// Google Apps Script Web App URL
+// 請將此 URL 替換為您部署的 Google Apps Script Web App URL
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzeAZjeeu-i_zxZ7yqtMhF-GlqRolONshl3DVckvv9jNxdWlvUSSnscDaAeVfLUQ7Ss/exec';
+
+// Google OAuth Client ID
+// 請前往 https://console.cloud.google.com/apis/credentials 建立 OAuth 2.0 客戶端 ID
+const GOOGLE_CLIENT_ID = '487197553483-ob3hv43l2kkvunkc0nqc9bd3kj6f8v8g.apps.googleusercontent.com';
+
